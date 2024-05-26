31e105f4d3b2ed4b14c178a91c0acb183acbd55b
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2024-05-26 18:34:16 +0100

    New changes added

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-var canvas = document.getElementById=("the_canvas");
-var ctx = canvas.getContext("2d");
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 6000);
+} 
