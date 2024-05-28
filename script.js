00f528ae9dd23c080c79ab12ca4382227bacbaa2
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2024-05-29 00:46:47 +0100

    animations added with JS

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let slideIndex = 0;
 let menuLinks = document.getElementById("menu-links");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.toggle('vidim', entry.isIntersecting);
+        }
+
+        if (!entry.isIntersecting) {
+            entry.target.classList.remove('vidim');
+        }
+    });
+});
+
+const hiddenCards = document.querySelectorAll('.cards');
+const hiddenText = document.querySelectorAll('.title-text');
+hiddenCards.forEach((el) => observer.observe(el));
+hiddenText.forEach((el) => observer.observe(el));
+
+
+
 showSlides();
 
 function showSlides() {
