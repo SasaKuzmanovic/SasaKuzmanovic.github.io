64fbded62614fa71ebb66951a43b1cf3b22dbbb1
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2024-06-14 17:15:21 +0100

    pwa install help thingy

--- js/script.js
+++ js/script.js
@@ -57,3 +57,26 @@ hiddenPayables.forEach((el) => observer.observe(el));
 function toggleMenu() {
     menuLinks.classList.toggle('show-menu');
 }
+
+
+// Installation
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+
+installButton.addEventListener("click", async () => {
+    if (!installPrompt) {
+      return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  });
